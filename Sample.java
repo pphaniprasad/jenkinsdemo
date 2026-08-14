88c550456876e043876d4c77ead599ff9887dcdf
pphaniprasad <phaniprasad_cse@mvsrec.edu.in>
2026-08-14 06:40:37 +0530

    firstSample

---
+++ Sample.java
@@ -0,0 +1,9 @@
+class Sample
+  {
+    public static void main(String args[])
+    {
+      System.out.println("Java program executed");
+      int a=50,b=60;
+      System.out.println("Result="+(a+b));
+    }
+  }
